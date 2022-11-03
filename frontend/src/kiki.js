0ea54acb7bc40e9067ca21df53d8828936dfb952
JAVIER <javierpia0105@gmail.com>
2022-11-03 17:50:30 -0300

    FN inicio con amazona

---
+++ frontend/src/kiki.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function kiki() {
+  return <div>kiki</div>;
+}
+
+export default kiki;
+
+import React from 'react'
+
+function kiki1() {
+  return (
+    <div>
+      kiki1
+    </div>
+kiki1  )
+}
+
+export default kiki1
